94aacaddbc75df8eb00c383e022bba19a57842e6
Stav Geffen <stavgeffen@gmail.com>
2016-12-04 15:27:11 +0200

    fix(server): ensures maxAge:0 for all requests

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const server = express();
 
-server.get('/', (req, res) => res.sendFile('index.html', { root: 'public' }));
+server.get('/', (req, res) => res.sendFile('index.html', { root: 'public', maxAge: 0 }));
 
 // serve static files
-server.use(express.static('public'));
+server.use(express.static('public', { maxAge: 0 }));
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
