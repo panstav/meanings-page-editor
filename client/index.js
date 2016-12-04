50fcd393f2b4a78ad4ea2582de086218b4e10acb
Stav Geffen <stavgeffen@gmail.com>
2016-12-04 15:22:22 +0200

    fix(interaction): removes all contenteditable attributes

--- client/index.js
+++ client/index.js
@@ -10,20 +10,20 @@ function renderCategories(){
 
 	function categoryElem(category){
 
-		return `<div id="cat-${category.namespace}" class="mt5">
+		return `<section id="cat-${category.namespace}" class="mt5">
 	<header class="relative">
 		<img src="https://loremflickr.com/g/640/240/${category.namespace}" class="w-100">
 		<h2 class="absolute pr4 pv3 ma0 bottom-0 right-0 left-0 light-gray">${category.title}</h2>
 	</header>
 	<ul class="list pa0">${category.items.map(categoryItemElem).join('')}</ul>
-</div>`;
+</section>`;
 
 	}
 
 	function categoryItemElem(item){
 
 		return `<li class="mv2">
-	<details contenteditable="true" class="outline-0">
+	<details class="outline-0">
 		<summary class="pointer">${item.title}</summary>
 		${itemContentElem(item.content)}
 	</details>
