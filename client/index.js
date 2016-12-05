ecacb7e7c16ac6fc243cf2428dea1fdf1cecf1cf
Stav Geffen <stavgeffen@gmail.com>
2016-12-05 14:39:20 +0200

    fix(typography): sets bigger font-size for category-titles and category-item-titles

--- client/index.js
+++ client/index.js
@@ -13,7 +13,7 @@ function renderCategories(){
 		return `<section id="cat-${category.namespace}" class="mt5">
 	<header class="relative overflow-hidden">
 		<img src="https://loremflickr.com/g/640/240/${category.namespace}" class="w-100">
-		<h2 class="absolute pr4 pv3 ma0 bottom-0 right-0 left-0 light-gray">${category.title}</h2>
+		<h2 class="absolute f2 pr4 pv3 ma0 bottom-0 right-0 left-0 light-gray">${category.title}</h2>
 	</header>
 	<ul class="list pa0">${category.items.map(categoryItemElem).join('')}</ul>
 </section>`;
@@ -24,7 +24,7 @@ function renderCategories(){
 
 		return `<li class="mv2">
 	<details>
-		<summary class="outline-0 pointer">${item.title}</summary>
+		<summary class="f3 outline-0 pointer">${item.title}</summary>
 		${itemContentElem(item.content)}
 	</details>
 </li>`
